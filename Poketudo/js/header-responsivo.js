613d3a30bc087fac453f27a5fc038db436394aff
KingNinhoCode <54991575+KingNinhoCode@users.noreply.github.com>
2020-03-25 20:00:37 -0300

    Initial commit

---
+++ Poketudo/js/header-responsivo.js
@@ -0,0 +1,33 @@
+var menu = document.querySelector(".logo");
+var nav = document.querySelector("nav");
+var header = document.querySelector("header");
+var overlay;
+var side = false;
+
+if (window.innerWidth <= 590) {
+
+	menu.classList.add("clicavel");
+
+}
+
+menu.addEventListener("click", function(event) {
+
+	if (window.innerWidth <= 590) {
+
+		nav.classList.add("side")
+		side = true;
+
+		overlay = document.createElement("div")
+		overlay.classList.add("overlay")
+		header.appendChild(overlay)
+	}
+})
+
+document.addEventListener("click", function(event){
+	
+	if (side = true && event.x > nav.offsetWidth) {
+		nav.classList.remove("side")
+		header.removeChild(overlay)
+		side = false
+	}
+})
